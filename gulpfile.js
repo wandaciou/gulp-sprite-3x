881edf9710ab373413132c63f06e96b306e8418e
wanting <wandaciou@gmail.com>
2021-03-16 18:12:21 +0800

    not support 3x

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ gulp.task('sprite', function () {
   var spriteData = gulp.src('dist/img/*.png').pipe(spritesmith({
       retinaSrcFilter: 'dist/img/*@2x.png',
       retinaImgName: 'sprite@2x.png',
-      retina3xSrcFilter: '/dist/img/*@3x.png',
-      retina3xImgName: 'sprite@3x.png',
       imgName: 'sprite.png',
       cssName: 'sprite.css'
   }));
