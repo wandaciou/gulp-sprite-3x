b8ccc690ca89b4286125b5f75f0668432eef8692
wanting <wandaciou@gmail.com>
2021-03-16 18:03:59 +0800

    2x support

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ var spritesmith = require('gulp.spritesmith');
 
 gulp.task('sprite', function () {
   var spriteData = gulp.src('dist/img/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: 'sprite.css'
+      retinaSrcFilter: 'dist/img/*@2x.png',
+      retinaImgName: 'sprite@2x.png',
+      retina3xSrcFilter: '/dist/img/*@3x.png',
+      retina3xImgName: 'sprite@3x.png',
+      imgName: 'sprite.png',
+      cssName: 'sprite.css'
   }));
   return spriteData.pipe(gulp.dest('dist/'));
 });
