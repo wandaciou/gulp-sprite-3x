5f74f3ac58da81c6d027683b1aebdd07f6c00a13
wanting <wandaciou@gmail.com>
2021-03-16 18:15:49 +0800

    support 3x

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
+var sprite = require('gulp.spritesmith.3x');
 
-gulp.task('sprite', function () {
-  var spriteData = gulp.src('dist/img/*.png').pipe(spritesmith({
+gulp.task('sprite', function() {
+  var spriteData = gulp.src('dist/img/*.png')
+    .pipe(sprite({
       retinaSrcFilter: 'dist/img/*@2x.png',
       retinaImgName: 'sprite@2x.png',
+      retina3xSrcFilter: 'dist/img/*@3x.png',
+      retina3xImgName: 'sprite@3x.png',
       imgName: 'sprite.png',
-      cssName: 'sprite.css'
-  }));
-  return spriteData.pipe(gulp.dest('dist/'));
+      imgPath: '../img/sprite.png',
+      retinaImgPath: '../img/sprite@2x.png',
+      retina3xImgPath: '../img/sprite@3x.png',
+      cssName: 'sprites.css'
+    }));
+
+  spriteData.img.pipe(gulp.dest('./dist/img/'));
+  spriteData.css.pipe(gulp.dest('./sass/'));
 });
