e245f34d890194ce46ca6abb38ca53e71f5a617a
wanting <g3024@evertrust.com.tw>
2021-03-17 11:57:47 +0800

    整理

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,16 @@ var sprite = require('gulp.spritesmith.3x');
 gulp.task('sprite', function() {
   var spriteData = gulp.src('dist/img/icon/*.png')
     .pipe(sprite({
-      retinaSrcFilter: 'dist/img/icon/*@2x.png',
-      retinaImgName: 'sprite@2x.png',
-      retina3xSrcFilter: 'dist/img/icon/*@3x.png',
-      retina3xImgName: 'sprite@3x.png',
       imgName: 'sprite.png',
+      retinaImgName: 'sprite@2x.png',
+      retina3xImgName: 'sprite@3x.png',
+      algorithms: 'left-right',
+      cssName: 'sprite.css',
+      retinaSrcFilter: 'dist/img/icon/*@2x.png',
+      retina3xSrcFilter: 'dist/img/icon/*@3x.png',
       imgPath: '../img/sprite/sprite.png',
       retinaImgPath: '../img/sprite/sprite@2x.png',
-      retina3xImgPath: '../img/sprite/sprite@3x.png',
-      cssName: 'sprite.css'
+      retina3xImgPath: '../img/sprite/sprite@3x.png'
     }));
 
   spriteData.img.pipe(gulp.dest('./dist/img/sprite/'));
--- readme.md
+++ readme.md
@@ -3,4 +3,7 @@ $npm install
 $gulp sprite
 
 使用
-https://www.npmjs.com/package/gulp.spritesmith.3x
+https://www.npmjs.com/package/gulp.spritesmith.
+
+前端处理小图标解决方案
+https://www.jianshu.com/p/650f9dacf4eb
