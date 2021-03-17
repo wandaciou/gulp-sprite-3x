6724a259759cf483e47bdb1c1bda738abe07dac9
wanting <wandaciou@gmail.com>
2021-03-17 09:52:24 +0800

    修改路徑

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sprite = require('gulp.spritesmith.3x');
 
 gulp.task('sprite', function() {
-  var spriteData = gulp.src('dist/img/*.png')
+  var spriteData = gulp.src('dist/img/icon*.png')
     .pipe(sprite({
-      retinaSrcFilter: 'dist/img/*@2x.png',
+      retinaSrcFilter: 'dist/img/icon/*@2x.png',
       retinaImgName: 'sprite@2x.png',
-      retina3xSrcFilter: 'dist/img/*@3x.png',
+      retina3xSrcFilter: 'dist/img/icon/*@3x.png',
       retina3xImgName: 'sprite@3x.png',
       imgName: 'sprite.png',
       imgPath: '../img/sprite/sprite.png',
